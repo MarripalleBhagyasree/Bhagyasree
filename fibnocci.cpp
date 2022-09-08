5d7351bdfbfd8cd7262418c772f0db7f3ce6894d
MarripalleBhagyasree <112714128+MarripalleBhagyasree@users.noreply.github.com>
2022-09-08 12:29:20 +0530

    Update fibnocci.cpp

--- fibnocci.cpp
+++ fibnocci.cpp
@@ -1,16 +1,27 @@
-#include<stdio.h>
+include<iostream>
+using namespace std;
 int main()
 {
-	int i,n,t1=0,t2=1,x;
-	printf("enter the number of terms :");
-	scanf("%d",&n);
-	printf("fibnacci series");
-	for(i=1;i<=n;i++)
-{
-   printf("%d\n",t1);
-   x=t1+t2;
-   t1=t2;
-   t2=x;
-}
-return 0;
+	int n,t1=0,t2=0,nextterm=0;
+	cout<<"Enter the number of terms:";
+	cin>>n;
+	cout<<"fibonacci series:";
+	for(int i=1;i<=n;++i)
+	{
+		if(i==1)
+		{
+			cout<<t1<<", ";
+			continue;
+		}
+		if(i==2)
+		{
+			cout<<t2<<", ";
+			continue;
+		}
+		nexttrem=t1+t2;
+		t1=t2;
+		t2=nextterm;
+		cout<<nextterm<<", ";
+	}
+	return 0;
 }
